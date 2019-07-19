e5a1cbcd6b2a0f1c806903aa9619f53cb02766b1
onevcat <onevcat@gmail.com>
2019-07-19 14:08:36 +0900

    Add test targets

--- Carthage/Carthage/ViewController.swift
+++ Carthage/Carthage/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        LoginManager.shared.login { result in
+            print(result)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Carthage/CarthageTests/CarthageTests.swift
@@ -0,0 +1,29 @@
+//
+//  CarthageTests.swift
+//  CarthageTests
+//
+//  Created by Wang Wei on 2019/07/19.
+//  Copyright © 2019 LINE Corp. All rights reserved.
+//
+
+import XCTest
+import LineSDK
+
+class CarthageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let version = LineSDK.Constant.SDKVersion
+        XCTAssertFalse(version.isEmpty)
+
+    }
+}
