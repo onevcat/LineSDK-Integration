6b9412df93f6bd8384a7c77b4dc480210e1c68ad
onevcat <onevcat@gmail.com>
2019-07-24 10:26:40 +0900

    Add binary framework test proj

---
+++ BinaryFramework/BinaryFramework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinaryFramework
+//
+//  Created by Wang Wei on 2019/07/24.
+//  Copyright © 2019 OneV's Den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
